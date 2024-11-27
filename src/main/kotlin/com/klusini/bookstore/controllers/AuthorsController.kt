1fd236c61d94ac2783d20d475b76002d08ae1529
Klusini <157594118+Klusini@users.noreply.github.com>
2024-11-27 17:48:05 +0100

    Read one author endpoint

--- src/main/kotlin/com/klusini/bookstore/controllers/AuthorsController.kt
+++ src/main/kotlin/com/klusini/bookstore/controllers/AuthorsController.kt
@@ -7,6 +7,7 @@ import com.klusini.bookstore.toAuthorEntity
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -27,6 +28,14 @@ class AuthorsController(private val authorService: AuthorService){
     @GetMapping
     fun readManyAuthors(): List<AuthorDto> {
         return authorService.list().map { it.toAuthorDto() }
+    }
+
+    @GetMapping(path = ["/{id}"])
+    fun readyOneAuthor(@PathVariable("id") id: Long): ResponseEntity<AuthorDto> {
+        val foundAuthor = authorService.get(id)?.toAuthorDto()
+        return foundAuthor?.let {
+            ResponseEntity(it, HttpStatus.OK)
+        } ?: ResponseEntity(HttpStatus.NOT_FOUND)
     }
 
 }
--- src/main/kotlin/com/klusini/bookstore/services/impl/AuthorServiceImpl.kt
+++ src/main/kotlin/com/klusini/bookstore/services/impl/AuthorServiceImpl.kt
@@ -3,6 +3,7 @@ package com.klusini.bookstore.services.impl
 import com.klusini.bookstore.domain.entities.AuthorEntity
 import com.klusini.bookstore.repositories.AuthorRepository
 import com.klusini.bookstore.services.AuthorService
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
@@ -14,6 +15,10 @@ class AuthorServiceImpl(private val authorRepository: AuthorRepository ) : Autho
 
     override fun list(): List<AuthorEntity> {
         return authorRepository.findAll()
+    }
+
+    override fun get(id: Long): AuthorEntity? {
+        return authorRepository.findByIdOrNull(id)
     }
 
 }
